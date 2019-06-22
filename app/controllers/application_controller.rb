4508b0d79d0408ab45bfb1402faa8e4d3f4b3d4e
Emily <cookie0915n@yahoo.co.jp>
2019-06-22 15:13:41 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+      # redirect_to "http://www.yahoo.co.jp/"
+    end
+  end
+
+  def counts(user)
+    @count_tasks = user.tasks.count
+  end
 end
--- db/migrate/20190622142437_create_tasks.rb
+++ db/migrate/20190622142437_create_tasks.rb
@@ -3,9 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :content
       t.references :user, foreign_key: true
-      t.boolean :done
-      t.int :priority
-      t.date :date
+      t.integer :priority
 
       t.timestamps
     end
